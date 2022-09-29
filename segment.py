1886f052b8eb2d632f60a188e7f0618fb5a48fd9
Josh Williams <jw144@hw.ac.uk>
2022-09-29 08:15:47 +0100

    [refactor] major cleanup of segment.py
    Tidied up how files are read - now use argparse to point to a single directory
    then can loop over various args with a bash script (keep python script simple).
    Remove unused packages.
    Reformat with cblack

--- segment.py
+++ segment.py
@@ -1,100 +1,84 @@
-import numpy as np
-import pickle
-import nrrd 
-import hjson
-import os
-import torch
-import SimpleITK as sitk
-from data.seg_dataset import resampleImage
-from torch.utils import data
-import torch.optim as optim
-import torch.nn as nn
-from torch.nn import functional as F
-from glob import glob
-from sys import exit
-import nrrd
+import argparse
 from os import mkdir
+from sys import exit
 
+import hjson
 import model
+import numpy as np
+import SimpleITK as sitk
+import torch
+import vedo as v
 from data import *
+
+
+def inputs():
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument(
+    "-i",
+    "--ct_path",
+    required=True,
+    type=str,
+    help="input image to segment /path/to/ct/*.mhd or /path/to/dicom/",
+  )
+  parser.add_argument(
+    "-o",
+    "--output_surface",
+    default="seg-1.stl",
+    type=str,
+    help="Airway segmentation surface file name",
+  )
+
+  return parser.parse_args()
+
+
+args = inputs()
 
 with open("config.json") as f:
   config = hjson.load(f)
 
 device = torch.device("cpu")
+#mkdir("segmentations", exist_ok=True)
 
-if len(glob('segmentations/')) == 0:
-    mkdir('segmentations')
+criterion = utils.lossTang2019  # utils.dice_loss
+unet = model.UNet(
+  1, config["train3d"]["n_classes"], config["train3d"]["start_filters"]
+).to(device)
+unet.load_state_dict(torch.load("./unet-model.pt"))
 
-#list_scans = glob(config["path"]["test_scans"])
-list_scans = glob(config["path"]["test_scans"]) #-Lists all .mhd scans.
+dataset = seg_dataset.SegmentSet(
+  [args.ct_path],
+  crop_fraction=config["segment3d"]["crop_fraction"],
+)
 
-#st_scans = [os.path.basename(s) for s in list_scans]
+# get data item i
+X, X_orig = dataset.__getitem__(0)
 
-print("st_scans", list_scans)
+# format data to be read by NN
+X = torch.Tensor(np.array([X.astype(np.float16)])).to(device)  # scan
+logits = unet(X)
+labelMap = torch.max(logits[0], 0)[1].numpy()
+labelMap = np.swapaxes(labelMap, 0, 2)
 
+segmentation = [None] * len([1])
+for l, label in enumerate([1]):
+  segmentation[l] = utils.extract_largest_island(labelMap)
 
+  # get metadata from original ct dataset
+  # segmentation[l].SetOrigin(X_orig.GetOrigin())
+  # segmentation[l].SetSpacing(X_orig.GetSpacing())
+  # add extra layer of voxels to make closed surface
+  # pad = sitk.ConstantPadImageFilter()
+  # pad.SetPadLowerBound((1, 1, 1))
+  # pad.SetPadUpperBound((1, 1, 1))
+  # pad.SetConstant(0)
+  # segmentation[l] = pad.Execute(segmentation[l])
 
-#dataset = seg_dataset.SegmentSet(list_scans, config["path"]["test_scans"], mode="3d", scan_size=config["train3d"]["scan_size"])
-
-print(config["path"]["test_scans"])
-
-criterion = utils.lossTang2019 #utils.dice_loss
-unet = model.UNet(1,config["train3d"]["n_classes"], config["train3d"]["start_filters"]).to(device)
-unet.load_state_dict(torch.load("./model.pt"))
-
-writeNRRDlib = False
-writeSITK = True
-for i in range(len(list_scans)):
-    dataset = seg_dataset.SegmentSet(list_scans[i], list_scans[i], mode="3d", scan_size=config["train3d"]["scan_size"], crop_fraction=config['segment3d']['crop_fraction'])
-    segID = str(list_scans[i].replace(".mhd", ""))[-4:]
-    if 'EXACT09' in list_scans[i]:
-        segID = 'CASE'+list_scans[i].split('CASE')[1][:2]
-    print("-"*30,"Getting and pre-processing scan")
-    print('writeID is', segID)
-    # get data item i
-    X, X_orig = dataset.__getitem__(i)
-    print('X shape is',X.shape)
-    # format data to be read by NN
-    X = torch.Tensor(np.array([X.astype(np.float16)])).to(device) #scan
-    print("-"*30, "Getting probabilities")
-    logits = unet(X)
-    print("-"*30, "Getting label map")
-    labelMap = torch.max(logits[0],0)[1].numpy()
-    print('shape is', labelMap.shape)
-    labelMap = np.swapaxes(labelMap, 0, 2)
-    print("-"*30, "Getting largest island for each label")
-    segmentation = [None]*len([1])
-    for l, label in enumerate([1]):
-        segmentation[l] = utils.getLargestIsland(labelMap==label)
-        # change shape to correct orientation for comparing with ground truths
-        segmentation[l] = sitk.GetImageFromArray(np.array(segmentation[l],dtype=np.int8))
-        segmentation[l] = sitk.GetArrayFromImage(segmentation[l])
-        segmentation[l] = np.swapaxes(segmentation[l], 1, 2)
-        segmentation[l] = np.swapaxes(segmentation[l], 0, 2)
-        segmentation[l] = np.swapaxes(segmentation[l], 1, 0)
-        print("-"*30, "Upsampling label map", label)
-        segmentation[l] = sitk.GetImageFromArray(segmentation[l])
-        segmentation[l] = resampleImage(segmentation[l], 
-                                        X_orig.GetSize(), 
-                                        interpolator=sitk.sitkNearestNeighbor)
-        # get metadata from original ct dataset
-        segmentation[l].SetOrigin(X_orig.GetOrigin())
-        segmentation[l].SetSpacing(X_orig.GetSpacing())
-        # add extra layer of voxels to make closed surface
-        pad = sitk.ConstantPadImageFilter()
-        pad.SetPadLowerBound((1,1,1))
-        pad.SetPadUpperBound((1,1,1))
-        pad.SetConstant(0)
-        segmentation[l] = pad.Execute(segmentation[l])
-        print("-"*30, "Writing label map", label)
-        # output data
-        sitk.WriteImage(segmentation[l], "./segmentations/seg-{0}-{1}.mhd".format(segID, "aw"))
-        utils.binaryLabelToSTL("./segmentations/seg-{0}-{1}.mhd".format(segID, "aw"),
-                    "./segmentations/{0}_mm_{1}.stl".format(segID, "aw"))
-
-#spacing = [str(X_orig.GetSpacing()[0]), str(X_orig.GetSpacing()[1]), str(X_orig.GetSpacing()[2])]
-#centre =  ['"'+str(X_orig.GetOrigin()[0])+'"', '"'+str(X_orig.GetOrigin()[1])+'"', '"'+str(X_orig.GetOrigin()[2])+'"']
-#header = {'kinds': ['domain', 'domain', 'domain'], 'units': ['mm', 'mm', 'mm'], 'spacings': spacing, 'space': 'right-anterior-superior', 'centerings':centre}
-#nrrd.write("mask3D_segBin.nrrd", labelArr, header, compression_level=9)
-
+  vol = v.Volume(
+    np.pad(segmentation[l].T, 1),
+    spacing=X_orig.GetSpacing(),
+    origin=X_orig.GetOrigin(),
+  )
+  print(f"isosurface spacing {X_orig.GetSpacing()}")
+  print("vol to isosurface")
+  mesh = vol.isosurface()
+  v.write(mesh, args.output_surface)
